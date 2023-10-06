bb3fbc76f6e35b9387cd1e02556bd7b44f4df07f
lcordoval <lcordova@multiplied.com>
2023-10-05 21:51:33 -0500

    shopping card disenio

---
+++ SolECommerce/SolECommerce/Controllers/ShoppingCarController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SolECommerce.Controllers
+{
+    public class ShoppingCarController : Controller
+    {
+        public IActionResult ProductCatalog()
+        {
+            return View();
+        }
+
+        public IActionResult ProductCategoryDetail(int id) {
+            return View();
+        }
+
+
+        public IActionResult ConfirmShoppingCar()
+        {
+            return View();
+        }
+
+    }
+}
